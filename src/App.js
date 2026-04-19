302a2e487e794c5184e49b82153d6812d65cb896
mathcorner7-design <mathcorner7@gmail.com>
2026-04-19 08:17:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app); 
 const auth = getAuth(app); // Auth instance
 const provider = new GoogleAuthProvider();
-
+provider.setCustomParameters({ prompt: 'select_account' });
 // --- 🔵 Countdown Component for Live Mocks --- 
 const LiveCountdown = ({ timestamp, onExpire }) => { 
   const [timeLeft, setTimeLeft] = useState(""); 
