0e97d90ceb1bdbe600eb9671944781e9d29b19da
mathcorner7-design <mathcorner7@gmail.com>
2026-04-28 15:04:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -988,12 +988,13 @@ const InteractiveExamHall = ({ exam, onFinish, studentsList, setIsAppSubmitting 
       img.src = event.target.result;
       img.onload = () => {
         const canvas = document.createElement('canvas');
-        const MAX_WIDTH = 800;
+        const MAX_WIDTH = 500;
         canvas.width = MAX_WIDTH;
         canvas.height = img.height * (MAX_WIDTH / img.width);
         const ctx = canvas.getContext('2d');
+        ctx.filter = "grayscale(100%) contrast(120%)";
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-        const compressedBase64 = canvas.toDataURL('image/jpeg', 0.2);
+        const compressedBase64 = canvas.toDataURL('image/jpeg', 0.3);
         setAnswers(prev => {
           const existingPhotos = Array.isArray(prev[qNum]) ? prev[qNum] : [];
           return { ...prev, [qNum]: [...existingPhotos, compressedBase64] };
